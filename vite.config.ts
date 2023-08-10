6c6a612ab7b90d08430c6fccbdd65c3b5717fbfa
theonlytails <theonlytails@theonlytails.com>
2023-08-10 23:06:10 +0300

    switched to vite in library mode from unbuild

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite"
+import { resolve } from "path"
+import dts from "vite-plugin-dts"
+
+export default defineConfig({
+	build: {
+		lib: {
+			entry: resolve(__dirname, "src/index.ts"),
+			name: "rehypeColorPreview",
+		},
+	},
+	plugins: [dts()],
+})
